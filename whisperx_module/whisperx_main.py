9797b0727e03809dc3481b130eb75743c79be9ca
ahmad-thewhiz <inboxofahmad@gmail.com>
2024-04-19 09:43:42 -0700

    Added WhisperX

---
+++ whisperx_module/whisperx_main.py
@@ -0,0 +1,20 @@
+import whisperx
+import gc 
+
+def get_transcription(path: str):
+    device = "cpu" 
+    audio_file = path
+    batch_size = 8
+    compute_type = "int8"
+
+    output = ""
+    try:
+        model = whisperx.load_model("large-v2", device, compute_type=compute_type)
+
+        audio = whisperx.load_audio(audio_file)
+        result = model.transcribe(audio, batch_size=batch_size)
+        output = result["segments"]
+        del model
+    except Exception as e:
+        output = "Error in whisperx_main.get_transcription: {e}"
+    return output
